82582b348b5f2fe619d20fce54db506389e989ce
Marco Noronha <1246670@DESKTOP-72GJ4R4>
2022-08-21 17:35:09 -0300

    Cleaning cloned repo

--- fonte-antigo/webservices/01HelloWorldSparkFramework/src/main/java/com/ti2cc/HelloWorld.java
+++
@@ -1,9 +0,0 @@
-package com.ti2cc;
-
-import static spark.Spark.*;
-
-public class HelloWorld {
-    public static void main(String[] arg){
-        get("/hello", (request, response) -> "Hello World!");
-    }
-}
